02d8374077b6b737a52f4c383d2adae78764cd86
Perevertaylov Vasiliy <144835495+kum0o0@users.noreply.github.com>
2025-05-22 21:19:58 +0200

    Add files via upload

--- Laba3/task8.php
+++ Laba3/task8.php
@@ -1,44 +1,50 @@
 <?php
-function increaseEnthusiasm($anyString): string {
-    return $anyString . "!";
+
+// Функция для добавления восклицательного знака
+function increaseEnthusiasm($str) {
+    return $str . '!';
 }
 
-echo increaseEnthusiasm("My string") . "\n";
-
-function repeatThreeTimes($anyString): string {
-    return $anyString . $anyString . $anyString;
+// Функция для повторения строки три раза
+function repeatThreeTimes($str) {
+    return str_repeat($str, 3);
 }
 
-echo repeatThreeTimes("New string") . "\n";
-
-echo increaseEnthusiasm(repeatThreeTimes("I love PHP")) . "\n";
-
-function cut($anyString, $length = 10): string {
-    return substr($anyString, 0, $length);
+// Функция для обрезки строки
+function cut($str, $length = 228) {
+    return substr($str, 0, $length);
 }
 
-echo cut("djfkghdklfjshfk") . "\n";
-
-function printArrayRecursively($arr, $index = 0) {
-    if ($index < count($arr)) { 
-        echo $arr[$index] . " "; 
-        printArrayRecursively($arr, $index + 1);
+// Рекурсивный вывод элементов массива
+function printArrayRecursively($array, $index = 0) {
+    if ($index < count($array)) {
+        echo $array[$index] . "\n";
+        printArrayRecursively($array, $index + 1);
     }
 }
 
-$arr = [1, 2, 3, 4, 5];
-printArrayRecursively($arr);
-
-echo "\n";
-
-function sumDigitsRecursively($num) {
-    if ($num <= 9) {
-        return $num;
+// Суммирование цифр числа до однозначного числа
+function sumDigits($number) {
+    while ($number > 9) {
+        $sum = 0;
+        $digits = str_split((string)$number);
+        foreach ($digits as $digit) {
+            $sum += (int)$digit;
+        }
+        $number = $sum;
     }
-
-    return sumDigitsRecursively(array_sum(str_split($num)));
+    return $number;
 }
 
-echo sumDigitsRecursively(987); 
-echo "\n";
-echo sumDigitsRecursively(12345); 
+// Примеры использования функций
+echo increaseEnthusiasm("ELden ring?") . "\n";
+echo repeatThreeTimes("No") . "\n";
+echo increaseEnthusiasm(repeatThreeTimes("Dark Souls 2 2")) . "\n";
+echo cut("WOW!", 5) . "\n"; !
+
+$numbers = [5, 6, 9, 007, 10];
+printArrayRecursively($numbers); 
+
+echo sumDigits(69141) . "\n";
+
+?>
--- Laba3/task9.php
+++ Laba3/task9.php
@@ -1,46 +1,58 @@
 <?php
 
+
 $array = [];
-for ($i = 1; $i <= 5; $i++) {
+for ($i = 1; $i <= 20; $i++) {
     $array[] = str_repeat('x', $i);
 }
-print_r($array);
+print_r($array); 
 
-function arrayFill($value, $count): array {
-    $array = [];
-    for ($i = 0; $i < $count; $i++) {
-        $array[] = $value;
-    }
-    return $array;
+
+function arrayFill($value, $length) {
+    return array_fill(0, $length, $value);
 }
-print_r(arrayFill('x', 5));
 
-$numbers = [[1, 2, 3], [4, 5], [6]];
-$sum = array_sum(array_merge(...$numbers));
-echo "Сумма элементов массива: $sum\n";
+print_r(arrayFill('x', 5)); 
+
+
+$twoDimArray = [[1, 2, 3], [4, 5], [6]];
+$sum = 0;
+foreach ($twoDimArray as $subArray) {
+    $sum += array_sum($subArray);
+}
+echo $sum . "\n";
+
 
 $array = [];
+$counter = 1;
 for ($i = 0; $i < 3; $i++) {
-    for ($j = 1; $j <= 3; $j++) {
-        $array[$i][] = $i * 3 + $j;
+    $subArray = [];
+    for ($j = 0; $j < 3; $j++) {
+        $subArray[] = $counter++;
     }
+    $array[] = $subArray;
 }
-print_r($array);
+print_r($array); 
 
-$array = [2, 5, 3, 9];
-$result = $array[0] * $array[1] + $array[2] * $array[3];
-echo "Результат перемножения: $result\n";
 
-$user = ['name' => 'Кирилл', 'surname' => 'Куренков', 'patronymic' => 'Александрович'];
-echo $user['surname'] . ' ' . $user['name'] . ' ' . $user['patronymic'] . "\n";
+$array = [2, 6, 3, 52];
+$result = ($array[0] * $array[1]) + ($array[2] * $array[3]);
+echo $result . "\n"; 
+
+
+$user = ['name' => 'Esmanuelo', 'surname' => 'ElBruh', 'patronymic' => 'Peutrovich'];
+echo $user['surname'] . ' ' . $user['name'] . ' ' . $user['patronymic'] . "\n"; 
 
 
 $date = ['year' => date('Y'), 'month' => date('m'), 'day' => date('d')];
-echo $date['year'] . '-' . $date['month'] . '-' . $date['day'] . "\n";
+echo $date['year'] . '-' . $date['month'] . '-' . $date['day'] . "\n"; 
 
-$arr = ['a', 'b', 'c', 'd', 'e'];
-echo "Количество элементов массива: ", count($arr) . "\n";
 
-echo "Последний эллемент массива: ", end($arr) . "\n";
+$arr = ['a', 'b', 'c', 'd', 'E'];
+echo count($arr) . "\n";
 
-echo "Предпоследний элемент массива: ", prev($arr) . "\n";
+
+echo $arr[count($arr) - 1] . "\n"; 
+echo $arr[count($arr) - 2] . "\n"; 
+
+?>
